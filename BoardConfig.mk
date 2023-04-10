1ac57a88a2fa5dc06a37e594d3e56ea54569a696
Toygoon <toygoon@gmail.com>
2023-04-10 20:27:41 +0900

    halo: add device configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,15 +47,29 @@ TARGET_SCREEN_DENSITY := 480
 
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 4
+# vendor_boot.img-vendor_cmdline
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 bootconfig buildvariant=user
+# vendor_boot.img-pagesize
+BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := halo_defconfig
 TARGET_KERNEL_SOURCE := kernel/lenovo/halo
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+# boot.img-kernel
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+# vendor_boot.img-dtb
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+BOARD_INCLUDE_DTB_IN_BOOTIMG := 
+# 14.0.697_221216/dtbo.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_KERNEL_SEPARATED_DTBO := 
 endif
 
 # Partitions
@@ -98,3 +112,6 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := halo
